173c3c1c0bdf56fd1c6cfa26e9dbe1d3ae6947e5
mattdomingo <matthew.domingo2022@gmail.com>
2025-08-14 00:00:17 -0500

    hotfix: about scroll arrow

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -81,19 +81,7 @@ export default function AboutPage() {
             </div>
           </div>
         </div>
-
-        {/* Down Arrow */}
-        {currentSection === 0 && (
-          <button 
-            className="scroll-arrow scroll-arrow-down"
-            onClick={() => scrollToSection(1)}
-            aria-label="Scroll to Skills Inventory"
-          >
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M7 10l5 5 5-5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>
-          </button>
-        )}
+        
       </section>
 
       {/* Section 2: Skills Inventory */}
